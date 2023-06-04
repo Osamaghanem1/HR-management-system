4c86e7e415d1acf19cd9de98da42b9a3192fed8a
osamaghanem1 <osamamghanem@yahoo.com>
2023-06-04 19:36:04 +0000

    Adding the Constructor

--- app.js
+++ app.js
@@ -1,75 +1,20 @@
 'use strict';
-const employees = [
-  {
-    employeeID: 1000,
-    fullName: 'Ghazi Samer',
-    department: 'Administration',
-    level: 'Senior',
-    imageURL: 'https://www.corporatephotographylondon.com/wp-content/uploads/2019/11/HKstrategies-1029-1024x683.jpg ',
-    salary: 0
-  },
 
-  {
-    employeeID: 1001,
-    fullName: 'Lana Ali',
-    department: 'Finance',
-    level: 'Senior',
-    imageURL: 'https://as1.ftcdn.net/v2/jpg/02/64/61/02/1000_F_264610201_ba3OilZlMEctW1MjgQsYz2WS4OId6zhY.jpg',
-    salary: 0
-  },
-
-  {
-    employeeID: 1002,
-    fullName: 'Tamara Ayoub',
-    department: 'Marketing',
-    level: 'Senior',
-    imageURL: 'https://images.squarespace-cdn.com/content/v1/572e050c4d088ea3a8f0ac9d/1652567766986-KUSNHQRZEE0914MTGA14/OKelly%26Sorohan-47-PRINT.jpg?format=1000w',
-    salary: 0
-  },
-
-  {
-    employeeID: 1003,
-    fullName: 'Safi Walid',
-    department: 'Administration',
-    level: 'Mid-Senior',
-    imageURL: 'https://img.freepik.com/free-photo/close-up-confident-male-employee-white-collar-shirt-smiling-camera-standing-self-assured-against-studio-background_1258-26761.jpg?w=360',
-    salary: 0
-  },
-
-  {
-    employeeID: 1004,
-    fullName: 'Omar Zaid',
-    department: 'Development',
-    level: 'Senior',
-    imageURL: 'https://serenabolton.com/wp-content/uploads/2020/10/Headshots-West-London-Studio-01-3-1024x683.jpg',
-    salary: 0
-  },
-
-  {
-    employeeID: 1005,
-    fullName: 'Rana Saleh',
-    department: 'Development',
-    level: 'Junior',
-    imageURL: 'https://img.freepik.com/premium-photo/portrait-young-smiling-muslim-businesswoman-wearing-suit-with-hijab-white-background-studio_33799-11465.jpg',
-    salary: 0
-  },
-  {
-    employeeID: 1006,
-    fullName: 'Hadi Ahmad',
-    department: 'Finance',
-    level: 'Mid-Senior',
-    imageURL: 'https://i.pinimg.com/originals/cd/26/7d/cd267d9afa3343b50c468b3daec610bc.jpg',
-    salary: 0
-  }
-];
-
+// Constructor function for Employee
+function Employee(employeeID, fullName, department, level, imageURL, salary) {
+  this.employeeID = employeeID;
+  this.fullName = fullName;
+  this.department = department;
+  this.level = level;
+  this.imageURL = imageURL;
+  this.salary = salary;
+}
 
 function generateEmployeeID() {
   const min = 1000;
   const max = 9999;
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
 
 function calculateSalary(level) {
   let minSalary, maxSalary;
@@ -93,26 +38,34 @@ function calculateSalary(level) {
   }
 
   const salary = Math.floor(Math.random() * (maxSalary - minSalary + 1) + minSalary);
-  const netSalary = salary - (salary * 0.075); 
+  const netSalary = salary - (salary * 0.075);
 
   return { salary, netSalary };
 }
 
-function renderEmployees() {
-  const employeeList = document.getElementById('employee-list');
+Employee.prototype.render = function () {
+  document.write(`<li>
+    <p>Employee name: ${this.fullName}</p>
+    <p>Department: ${this.department}</p>
+    <p>Salary: ${this.salary}</p>
+  </li>`);
+};
 
-  employees.forEach((employee) => {
-    const { fullName, salary } = employee;
-    const listItem = document.createElement('li');
-    listItem.textContent = `Employee name: ${fullName}\n Employee salary: ${salary}`;
-    employeeList.appendChild(listItem);
-  });
-}
+const employees = [
+  new Employee(generateEmployeeID(), 'Ghazi Samer', 'Administration', 'Senior', 0),
+  new Employee(generateEmployeeID(), 'Lana Ali', 'Finance', 'Senior', 0),
+  new Employee(generateEmployeeID(), 'Tamara Ayoub', 'Marketing', 'Senior', 0),
+  new Employee(generateEmployeeID(), 'Safi Walid', 'Administration', 'Mid-Senior', 0),
+  new Employee(generateEmployeeID(), 'Omar Zaid', 'Development', 'Senior', 0),
+  new Employee(generateEmployeeID(), 'Rana Saleh', 'Development', 'Junior', 0),
+  new Employee(generateEmployeeID(), 'Hadi Ahmad', 'Finance', 'Mid-Senior', 0)
+];
 
 employees.forEach((employee) => {
-  employee.employeeID = generateEmployeeID();
   const { salary, netSalary } = calculateSalary(employee.level);
   employee.salary = netSalary;
 });
 
-renderEmployees();
+employees.forEach((employee) => {
+  employee.render();
+});
